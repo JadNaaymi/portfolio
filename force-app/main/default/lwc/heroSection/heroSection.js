099879ceaa54cf7a0ab4c909149a1ad2cdf342be
Jad Naaymi <jadnaaymi1@gmail.com>
2026-02-10 07:36:30 +0200

    changes in base and hero components

--- force-app/main/default/lwc/heroSection/heroSection.js
+++ force-app/main/default/lwc/heroSection/heroSection.js
@@ -2,4 +2,5 @@ import { LightningElement } from 'lwc';
 
 export default class HeroSection extends LightningElement {
    
+    memojiUrl = 'https://spyne-static.s3.amazonaws.com/Ai+Tool/car-transparent-bg-after-1.webp';
 }
